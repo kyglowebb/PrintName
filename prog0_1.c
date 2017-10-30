507b23dc0203ea9a3856591b045d556154b91766
Kylee Webb <kyleewebb7@gmail.com>
2017-10-29 23:56:55 -0700

    Add files via upload

---
+++ prog0_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) 
+{
+
+	char string[256];
+	char second[256];
+
+	printf("Assignment #0-1, Kylee Webb, kawebb@sdsu.edu\r\n");
+
+	printf("What is your name?\r\n");
+
+	scanf("%s %s", string, second);
+
+	printf("Hello %s %s\r\n", string, second);
+	
+
+	return 0;
+
+
+
+}
+
+
+
+
